3cca3d46b294412f4606dcc4b5fa34420403ca09
阿健 <uispot@qq.com>
2013-07-10 09:58:57 +0800

    file commit

---
+++ muban/js/loadindex.js
@@ -0,0 +1,9 @@
+function loadjs(file){ 
+	document.write("<script type='text/javascript' src='"+file+"'><\/script>");
+}
+loadjs("http://demo.wego360.com/statics/js/jquery/jquery-1.4.2.min.js");
+loadjs("http://demo.wego360.com/index/Tpl/default/public/js/base64.js");
+loadjs("js/common.js");
+
+
+
